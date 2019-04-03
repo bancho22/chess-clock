7648391a35aa67912999f57191c363c39c28b4a8
bancho_p <banchopetrov@gmail.com>
2019-04-04 00:39:15 +0200

    Timestamps are correctly formatted

--- src/app.presenter.jsx
+++ src/app.presenter.jsx
@@ -1,17 +1,31 @@
 import React from 'react'
 import {connect} from 'react-redux'
+import moment from 'moment'
+import zeroFill from 'zero-fill'
 import {updateCurrentPlayer} from './index.actions'
 import './index.css'
 
-const App = ({player, black, white, onButtonClick}) => (
-  <div className='app'>
-    <div className='container countdown'>
-      <span className={`countdown ${player ? player : ''}`}>White: {white}</span>
-      <span className={`countdown ${player ? player : ''}`}>Black: {black}</span>
+const App = ({player, black, white, onButtonClick}) => {
+  const whiteDuration = moment.duration(white)
+  const blackDuration = moment.duration(black)
+  return (
+    <div className='app'>
+      <div className='container countdown'>
+        <div className={`countdown ${player ? player : ''}`}>
+          <span>White</span>
+          <span>{zeroFill(2, whiteDuration.get('minutes'))}:{zeroFill(2, whiteDuration.get('seconds'))}</span>
+          <span>.{zeroFill(3, whiteDuration.get('milliseconds'))}</span>
+        </div>
+        <div className={`countdown ${player ? player : ''}`}>
+          <span>Black</span>
+          <span>{zeroFill(2, blackDuration.get('minutes'))}:{zeroFill(2, blackDuration.get('seconds'))}</span>
+          <span>.{zeroFill(3, blackDuration.get('milliseconds'))}</span>
+        </div>
+      </div>
+      <button className='container mainButton' onClick={() => onButtonClick(player)}>Button</button>
     </div>
-    <button className='container mainButton' onClick={() => onButtonClick(player)}>Button</button>
-  </div>
-)
+  )
+}
 
 const mapStateToProps = ({player, timeLeft: {black, white}}) => ({
   player,
